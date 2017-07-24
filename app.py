b3d423a924259735f166f88319bf72e11181198f
ArwaNayef <arwanayef2001@hotmail.com>
2017-07-24 15:55:35 +0300

    another changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app= Flask (__name__)
 		
 @app.route("/")
@@ -13,6 +13,19 @@ def healtyfood ():
 @app.route("/workout")
 def workout ():
 	return render_template("workout.html")
+
+@app.route("/contactus")
+def contactus ():
+	return render_template("contactus.html")
+
+@app.route("/contactus_response",methods= ['POST'])
+def contactushere():
+	user_message= request.form["messege"]
+	user_email= request.form["Email"]
+
+	return render_template("form_data.html", Email=user_email, message= user_message)
+
+
 
 if __name__ =="__main__":
 	app.run()
