a48a55ef1a451655c1b5c099517f2ebf9e0796d4
hala <halabarka00@gmail.com>
2017-07-25 14:49:10 +0300

    healthy food bage

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 from flask import Flask, render_template, request
 app= Flask (__name__)
-import dataset
+ 
 
-db=dataset.connectus( "postgres://yowahpfaxtsjoc:89e78de430594fa7bffec234ce5bc8ac9193cef0a864378fd900ddd573d65651@ec2-23-23-244-83.compute-1.amazonaws.com:5432/dbn2rcviq5s3i4") 
-table=db["user"]
-table.insert(dict(Email="halabarka11@gmail.com",message ="hi" )) 
-table.insert(dict(Email="hlabarka00@gmail.com",message ="hello" ))  
-
-for user in table:
-	print ("id :"+ str(user["id"])+"Email:"+user["Email"]+"message:"+str(user["message"]))
-	
+#db=dataset.connectus( "postgres://yowahpfaxtsjoc:89e78de430594fa7bffec234ce5bc8ac9193cef0a864378fd900ddd573d65651@ec2-23-23-244-83.compute-1.amazonaws.com:5432/dbn2rcviq5s3i4") 
+#recipes=db["food_recipes"] 
 @app.route("/")
 def home ():
 	return render_template("index.html")
@@ -32,7 +26,12 @@ def contactushere():
 	user_message= request.form["messege"]
 	user_email= request.form["Email"]
 
+ 	table.insert(dict(Email="hlabarka00@gmail.com",message ="hello" ))  
+ 
 	return render_template("form_data.html", Email=user_email, message= user_message)
+
+ 
+ 
 
 
 
