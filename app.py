3a710aa4862e736dcaf749b6f6dcd302cee4f18c
Arwa <arwanayef2001@hotmail.com>
2018-07-25 18:57:39 +0300

    data changes

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request
-import dataset
+#import dataset
 app= Flask (__name__)
 
 
-db= dataset.connect("postgres://yowahpfaxtsjoc:89e78de430594fa7bffec234ce5bc8ac9193cef0a864378fd900ddd573d65651@ec2-23-23-244-83.compute-1.amazonaws.com:5432/dbn2rcviq5s3i4")
+#db= dataset.connect("postgres://yowahpfaxtsjoc:89e78de430594fa7bffec234ce5bc8ac9193cef0a864378fd900ddd573d65651@ec2-23-23-244-83.compute-1.amazonaws.com:5432/dbn2rcviq5s3i4")
 
-recipes_table= db["food_recipes"]
+#recipes_table= db["food_recipes"]
 
 
 #{% for user in recipes %}
@@ -39,15 +39,15 @@ def yourrecipes ():
 	return render_template("yourrecipes.html", recipes=list(recipes))
 
 
-@app.route("/contactus_response",methods= ['POST'])
-def contactushere():
-	user_recipe= request.form["Recipe"]
-	user_email= request.form["Email"]
-	recipes_table.insert(dict(Email=user_email, Recipe=user_recipe))
+#@app.route("/contactus_response",methods= ['POST'])
+#def contactushere():
+#	user_recipe= request.form["Recipe"]
+#	user_email= request.form["Email"]
+#	recipes_table.insert(dict(Email=user_email, Recipe=user_recipe))
 
 
 
-	return render_template("form_data.html", Email=user_email, Recipe= user_recipe)
+#	return render_template("form_data.html", Email=user_email, Recipe= user_recipe)
 
 
 
